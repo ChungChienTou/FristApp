a54878a7e1cac29d3e8ff2c36449d1081fdb5b25
ChungChienToi <zx6r1988@gmail.com>
2019-03-07 18:30:19 +0800

    My first App

---
+++ FristApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FristApp
+//
+//  Created by ChungChien Tou on 2019/3/7.
+//  Copyright © 2019 ChungChien Tou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
